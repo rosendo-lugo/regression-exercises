39bfacfd464b19d548206be4217b0f335ae95d5e
Rosendo Lugo <rosendo.lugo@gmail.com>
2023-05-09 09:19:07 -0500

    Modified three files and added scaling lesson

--- wrangle.py
+++ wrangle.py
@@ -2,6 +2,17 @@ import pandas as pd
 from env import get_db_url
 import os
 
+# Stats
+from scipy import stats
+import sklearn.preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import QuantileTransformer
+
+
 
 def check_file_exists(fn, query, url):
     """
@@ -52,3 +63,91 @@ def get_zillow_data():
     df = df[df.taxvalue < df.taxvalue.quantile(.95)].copy()
     
     return df
+
+# ----------------------------------------------------------------------------------
+def get_zillow_split(df):
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.25, random_state=123)
+    
+    return train, validate, test
+
+
+# ----------------------------------------------------------------------------------
+
+def get_minmax_train_scaler(X_train, X_validate):
+    
+    columns=['bedrooms', 'bathrooms','area','yearbuilt','taxamount','tax_rate']
+    X_tr_mm_scaler = X_train.copy()
+    X_v_mm_scaler = X_validate.copy()
+    
+    # Create a MinMaxScaler object
+    mm_scaler = MinMaxScaler()
+
+    # Fit the scaler to the training data and transform it
+    X_tr_mm_scaler[columns] = mm_scaler.fit_transform(X_train[columns])
+    X_tr_mm_scaler = pd.DataFrame(X_tr_mm_scaler)
+    
+    #using our scaler on validate
+    X_v_mm_scaler[columns] = mm_scaler.transform(X_validate[columns])
+    X_v_mm_scaler = pd.DataFrame(X_v_mm_scaler)
+    
+    # inverse transform
+    X_tr_mm_inv = mm_scaler.inverse_transform(X_tr_mm_scaler[columns])
+    X_tr_mm_inv = pd.DataFrame(X_tr_mm_inv, columns=columns)
+
+    # add the county column to X_tr_mm_inv
+    X_tr_mm_inv = pd.concat([X_tr_mm_inv, X_train[['county']].reset_index(drop=True)], axis=1)
+    
+    return X_tr_mm_scaler, X_v_mm_scaler, X_tr_mm_inv
+
+# ----------------------------------------------------------------------------------
+def get_std_train_scaler(X_train, X_validate):
+    
+    columns=['bedrooms', 'bathrooms','area','yearbuilt','taxamount','tax_rate']
+    X_tr_std_scaler = X_train.copy()
+    X_v_std_scaler = X_validate.copy()
+    
+    # Create a MinMaxScaler object
+    std_scaler = StandardScaler()
+
+    # Fit the scaler to the training data and transform it
+    X_tr_std_scaler[columns] = std_scaler.fit_transform(X_train[columns])
+    
+    #using our scaler on validate
+    X_v_std_scaler[columns] = std_scaler.transform(X_validate[columns])
+    
+    return X_tr_std_scaler, X_v_std_scaler
+
+# ----------------------------------------------------------------------------------
+
+def get_robust_train_scaler(X_train, X_validate):
+     
+    columns=['bedrooms', 'bathrooms','area','yearbuilt','taxamount','tax_rate']
+    X_tr_rbs_scaler = X_train.copy()
+    X_v_rbs_scaler = X_validate.copy()
+    
+    # Create a MinMaxScaler object
+    rbs_scaler = RobustScaler()
+
+    # Fit the scaler to the training data and transform it
+    X_tr_rbs_scaler[columns] = rbs_scaler.fit_transform(X_train[columns])
+    
+    #using our scaler on validate
+    X_v_rbs_scaler[columns] = rbs_scaler.transform(X_validate[columns])
+    
+    return X_tr_rbs_scaler, X_v_rbs_scaler
+
+# ----------------------------------------------------------------------------------
+def get_quant_normal(X_train):
+    # Columns
+    columns=['bedrooms', 'bathrooms','area','yearbuilt','taxamount','tax_rate']
+    
+    # Quantile transform with output distribution "normal"
+    quant_norm = QuantileTransformer(output_distribution='normal')
+    X_tr_quant_norm = pd.DataFrame(quant_norm.fit_transform(X_train[columns]),columns=columns)
+    
+    # Quantile trainsform by it self
+    quant = QuantileTransformer()
+    X_tr_quant = pd.DataFrame(quant.fit_transform(X_train[columns]),columns=columns)
+    
+    return quant_norm, X_tr_quant_norm, quant, X_tr_quant
